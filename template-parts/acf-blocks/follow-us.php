7d614000826dbb032bb343c720c1f744fd08c2c2
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-06 16:45:09 +0200

    feat (follow us frontend)

--- template-parts/acf-blocks/follow-us.php
+++ template-parts/acf-blocks/follow-us.php
@@ -3,18 +3,26 @@ $title = get_sub_field('title');
 $icon = get_sub_field('icon');
 $feed_shortcode = get_sub_field('feed_shortcode');
 ?>
-<?php if($icon || $title): ?>
-    <div class="followUs__titleWrapper">
-        <?php if($title): ?>
-            <h2 class="followUs__title"><?php echo $title; ?></h2>
+<?php if($icon || $title && $feed_shortcode): ?>
+
+<div class="followUs">
+    <div class="container container--narrow">
+        <?php if($icon || $title): ?>
+            <div class="followUs__titleWrapper">
+                <?php if($title): ?>
+                    <h2 class="followUs__title"><?php echo $title; ?></h2>
+                <?php endif; ?>
+                <?php if($icon): ?>
+                    <img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['title']; ?>" class="followUs__icon">
+                <?php endif; ?>
+            </div>
         <?php endif; ?>
-        <?php if($icon): ?>
-            <img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['title']; ?>" class="followUs__icon">
+        <?php if($feed_shortcode): ?>
+            <div class="followUs__feed">
+                <?php echo do_shortcode( $feed_shortcode ); ?>
+            </div>
         <?php endif; ?>
     </div>
-<?php endif; ?>
-<?php if($feed_shortcode): ?>
-    <div class="followUs__feed">
-        <?php echo do_shortcode( $feed_shortcode ); ?>
-    </div>
+</div>
+
 <?php endif; ?>
