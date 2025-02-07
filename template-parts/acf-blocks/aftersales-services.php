16ee6b683f36d51008ce48f3593aabf3059261b2
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-08 00:35:46 +0200

    feat (aftersales services frontend)

--- template-parts/acf-blocks/aftersales-services.php
+++ template-parts/acf-blocks/aftersales-services.php
@@ -6,20 +6,27 @@ $args = array(
 );
 $the_query = new WP_Query($args);
 ?>
-<?php if($title): ?>
-    <h2 class="services__title"><?php echo $title; ?></h2>
-<?php endif; ?>
-<?php if($the_query->have_posts()): ?>
-    <div class="services__list">
-        <?php while($the_query->have_posts()): $the_query->the_post(); 
-            $image = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory() . '/assets/images/placeholder-image.webp';
-            ?>
-            <div class="services__listItem__wrapper">
-                <div class="services__listItem">
-                    <div class="services__listItem__title"><?php the_title(); ?></div>
-                    <div class="services__listItem__image"><img src="<?php echo $image; ?>" alt=""></div>
-                </div>
+<div class="services">
+    <div class="container">
+        <div class="services__wrapper">
+            <div class="container container--narrow">
+                <?php if($title): ?>
+                    <h2><?php echo $title; ?></h2>
+                <?php endif; ?>
+                <?php if($the_query->have_posts()): ?>
+                    <div class="services__list">
+                        <?php while($the_query->have_posts()): $the_query->the_post();
+                            $image = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory() . '/assets/images/placeholder-image.webp';
+                            ?>
+                            <div class="services__listItem">
+                                <h4><?php the_title(); ?></h4>
+                               <img src="<?php echo $image; ?>" alt="">
+                            </div>
+                        <?php endwhile; wp_reset_postdata(); ?>
+                    </div>
+                <?php endif; ?>
             </div>
-        <?php endwhile; wp_reset_postdata(); ?>
+        </div>
     </div>
-<?php endif; ?>
+</div>
+
