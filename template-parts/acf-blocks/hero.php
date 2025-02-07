4be2d748f67e606a7ec678b305457d0d78866a56
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-07 17:51:10 +0200

    feat (hero frontend)

--- template-parts/acf-blocks/hero.php
+++ template-parts/acf-blocks/hero.php
@@ -2,9 +2,17 @@
 $title = get_sub_field('title');
 $bg_image = get_sub_field('background_image');
 ?>
-<?php if($title): ?>
-    <h1 class="hero__title"><?php echo $title; ?></h1>
-<?php endif; ?>
-<?php if($bg_image): ?>
-    <div class="hero__image"><img src="<?php echo $bg_image['url']; ?>" alt="<?php echo $bg_image['title']; ?>"></div>
+<?php if($title || $bg_image): ?>
+
+<div class="hero">
+    <div class="hero__wrapper container container--wide">
+        <?php if($title): ?>
+            <h1 class="hero__title"><?php echo $title; ?></h1>
+        <?php endif; ?>
+        <?php if($bg_image): ?>
+            <div class="hero__image"><img src="<?php echo $bg_image['url']; ?>" alt="<?php echo $bg_image['title']; ?>"></div>
+        <?php endif; ?>
+    </div>
+</div>
+
 <?php endif; ?>
--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -1,7 +1,7 @@
 
 <div class="container">
 
-    <h1><?php the_title(); ?></h1>
+<!--    <h1>--><?php //the_title(); ?><!--</h1>-->
 
     <div class="content-block">
         <?php the_content(); ?>
