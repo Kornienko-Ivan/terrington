042bbafef81ec1977d19f5f1c229c5a9b036c551
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-10 11:28:59 +0200

    feat (review frontend)

--- template-parts/acf-blocks/aftersales-services.php
+++ template-parts/acf-blocks/aftersales-services.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $title = get_field('aftersales_services_title', 'options');
 $args = array(
     'post_type' => 'services',
@@ -11,17 +11,17 @@ $the_query = new WP_Query($args);
         <div class="services__wrapper">
             <div class="container container--narrow">
                 <?php if($title): ?>
-                    <h2><?php echo $title; ?></h2>
+                    <h2><?php echo esc_html($title); ?></h2>
                 <?php endif; ?>
                 <?php if($the_query->have_posts()): ?>
                     <div class="services__list">
                         <?php while($the_query->have_posts()): $the_query->the_post();
-                            $image = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory() . '/assets/images/placeholder-image.webp';
+                            $image = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
                             ?>
-                            <div class="services__listItem">
+                            <a href="<?php the_permalink(); ?>" class="services__listItem">
                                 <h4><?php the_title(); ?></h4>
-                               <img src="<?php echo $image; ?>" alt="">
-                            </div>
+                                <img src="<?php echo esc_url($image); ?>" alt="<?php the_title_attribute(); ?>">
+                            </a>
                         <?php endwhile; wp_reset_postdata(); ?>
                     </div>
                 <?php endif; ?>
@@ -29,4 +29,3 @@ $the_query = new WP_Query($args);
         </div>
     </div>
 </div>
-
--- template-parts/acf-blocks/review.php
+++ template-parts/acf-blocks/review.php
@@ -4,19 +4,34 @@ $title = get_sub_field('title');
 $text = get_sub_field('text');
 $button = get_sub_field('button');
 ?>
-<?php if($image): ?>
-    <div class="review__image"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>"></div>
-<?php endif; ?>
-<?php if($title || $text || $button): ?>
-    <div class="review__content">
-        <?php if($title): ?>
-            <h2 class="review__title"><?php echo $title; ?></h2>
-        <?php endif; ?>
-        <?php if($text): ?>
-            <div class="review__text"><?php echo $text; ?></div>
-        <?php endif; ?>
-        <?php if($button): ?>
-            <div class="review__button"><a href="<?php echo $button['url']; ?>" class="button"><?php echo $button['title']; ?></a></div>
-        <?php endif; ?>
+<?php if($image && $title): ?>
+
+<div class="review">
+    <div class="container container--narrow">
+        <div class="review__row">
+            <?php if($image): ?>
+                <div class="review__col review__col--image">
+                    <div class="review__image"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>"></div>
+                </div>
+            <?php endif; ?>
+            <?php if($title || $text || $button): ?>
+                <div class="review__col review__col--content">
+                    <div class="review__content">
+                        <?php if($title): ?>
+                            <h2 class="review__title"><?php echo $title; ?></h2>
+                        <?php endif; ?>
+                        <?php if($text): ?>
+                            <div class="review__text"><?php echo $text; ?></div>
+                        <?php endif; ?>
+                        <?php if($button): ?>
+                            <a href="<?php echo $button['url']; ?>" class="button"><?php echo $button['title']; ?></a>
+                        <?php endif; ?>
+                    </div>
+            </div>
+            <?php endif; ?>
+        </div>
     </div>
+
+</div>
 <?php endif; ?>
+
