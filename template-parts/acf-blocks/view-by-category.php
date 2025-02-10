96d11ed244ea21a0e8e18f0c355d49900503b04c
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-10 13:18:17 +0200

    feat (View by Category frontend)

--- template-parts/acf-blocks/view-by-category.php
+++ template-parts/acf-blocks/view-by-category.php
@@ -9,18 +9,20 @@ $terms = get_terms(
 );
 ?>
 <?php if($title || (!$terms->errors && !empty($terms))): ?>
-    <?php if($title): ?>
-        <h5 class="categories__title"><?php echo $title; ?></h5>
-    <?php endif; ?>
-    <?php if(!$terms->errors && !empty($terms)): ?>
-        <div class="categories__list">
-            <?php foreach($terms as $term): ?>
-            <div class="categories__listItem__wrapper">
-                <a href="<?php echo esc_url(get_term_link($term)); ?>" class="categories__listItem">
-                    <div class="categories__listItem__name"><?php echo $term->name; ?></div>
-                </a>
-            </div>
-            <?php endforeach; ?>
+    <div class="categories">
+        <div class="container container--narrow">
+                <?php if($title): ?>
+                    <h5><?php echo $title; ?></h5>
+                <?php endif; ?>
+                <?php if(!$terms->errors && !empty($terms)): ?>
+                    <div class="categories__list">
+                        <?php foreach($terms as $term): ?>
+                            <a href="<?php echo esc_url(get_term_link($term)); ?>" class="categories__listItem">
+                                <div class="categories__listItem__name"><?php echo $term->name; ?></div>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
         </div>
-    <?php endif; ?>
+    </div>
 <?php endif; ?>
