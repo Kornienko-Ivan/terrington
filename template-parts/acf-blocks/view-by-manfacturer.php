9f6773ef6d540f022a59ba80b4bf83b80dda51d9
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-10 12:35:24 +0200

    feat (View by Manufacturer frontend)

--- template-parts/acf-blocks/view-by-manfacturer.php
+++ template-parts/acf-blocks/view-by-manfacturer.php
@@ -6,27 +6,28 @@ $args = array(
 );
 $the_query = new WP_Query($args);
 ?>
-<?php if($title || $the_query->have_posts()): ?>
-    <?php if($title): ?>
-        <h5 class="manfacturers__title"><?php echo $title; ?></h5>
-    <?php endif; ?>
-    <?php if($the_query->have_posts()): ?>
-        <div class="manfacturers__list">
-            <?php while($the_query->have_posts()): $the_query->the_post(); ?>
-                <?php 
-                $bg = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
-                $logo = get_field('brand__image');
-                ?>
-                <div class="manfacturers__listItemWrapper">
-                    <a href="<?php the_permalink(); ?>" class="manfacturers__listItem">
-                        <div class="manfacturers__listItem__bg"><img src="<?php echo $bg; ?>" alt=""></div>
-                        <?php if($logo): ?>
-                            <div class="manfacturers__listItem__logo"><img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['title']; ?>"></div>
-                        <?php endif; ?>
-                        <div class="manfacturers__listItem__name"><?php the_title(); ?></div>
-                    </a>
+<div class="manfacturers">
+    <div class="container container--narrow">
+        <?php if($title || $the_query->have_posts()): ?>
+            <?php if($title): ?>
+                <h5><?php echo $title; ?></h5>
+            <?php endif; ?>
+            <?php if($the_query->have_posts()): ?>
+                <div class="manfacturers__list">
+                    <?php while($the_query->have_posts()): $the_query->the_post(); ?>
+                        <?php
+                        $bg = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
+                        $logo = get_field('brand__image');
+                        ?>
+                        <a href="<?php the_permalink(); ?>" class="manfacturers__listItem">
+                            <img src="<?php echo $bg; ?>" class="manfacturers__listItem__bg" alt="">
+                            <?php if($logo): ?>
+                               <img src="<?php echo $logo['url']; ?>" class="manfacturers__listItem__logo" alt="<?php echo $logo['title']; ?>">
+                            <?php endif; ?>
+                        </a>
+                    <?php endwhile; wp_reset_postdata(); ?>
                 </div>
-            <?php endwhile; wp_reset_postdata(); ?>
-        </div>
-    <?php endif; ?>
-<?php endif; ?>
+            <?php endif; ?>
+        <?php endif; ?>
+    </div>
+</div>
