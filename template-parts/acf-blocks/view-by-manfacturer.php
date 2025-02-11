f50b976b563fd7868af12a48cd47461a4fc3f77e
KornienkoIvan <ivan.k@codelibry.dev>
2025-02-11 14:37:16 +0200

    Fix: View By Manufacture Block

--- template-parts/acf-blocks/view-by-manfacturer.php
+++ template-parts/acf-blocks/view-by-manfacturer.php
@@ -16,13 +16,13 @@ $the_query = new WP_Query($args);
                 <div class="manfacturers__list">
                     <?php while($the_query->have_posts()): $the_query->the_post(); ?>
                         <?php
-                        $bg = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
-                        $logo = get_field('brand__image');
+                        $bg = get_field('brand__image') ? get_field('brand__image') : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
+                        $logo = get_the_post_thumbnail_url();
                         ?>
                         <a href="<?php the_permalink(); ?>" class="manfacturers__listItem">
-                            <img src="<?php echo $bg; ?>" class="manfacturers__listItem__bg" alt="">
+                            <img src="<?php echo $bg['url']; ?>" class="manfacturers__listItem__bg" alt="<?php echo $bg['title']; ?>">
                             <?php if($logo): ?>
-                               <img src="<?php echo $logo['url']; ?>" class="manfacturers__listItem__logo" alt="<?php echo $logo['title']; ?>">
+                               <img src="<?php echo $logo; ?>" class="manfacturers__listItem__logo" alt="<?php echo $logo; ?>">
                             <?php endif; ?>
                         </a>
                     <?php endwhile; wp_reset_postdata(); ?>
