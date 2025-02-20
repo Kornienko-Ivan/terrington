9125f6643519c3c556855a4e44f9b42edd747ae2
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-20 13:52:03 +0200

    feat (productText frontend)

--- template-parts/acf-blocks/single-product/text-block.php
+++ template-parts/acf-blocks/single-product/text-block.php
@@ -3,7 +3,7 @@ $text = get_sub_field('text');
 if($text):
 ?>
 <section class="productText">
-    <div class="container">
+    <div class="container container--narrow">
         <div class="productText__content"><?php echo $text; ?></div>
     </div>
 </section>
