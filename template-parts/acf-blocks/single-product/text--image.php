020eeb7e72831f0898de2f3d1e887f4f9f5e0037
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-20 12:53:43 +0200

    feat (productTextImage frontend)

--- template-parts/acf-blocks/single-product/text--image.php
+++ template-parts/acf-blocks/single-product/text--image.php
@@ -1,34 +1,41 @@
-<?php 
+<?php
+$spacing = get_sub_field('text__image_spacing') == 'small' ? 'productTextImage--small' : 'productTextImage--big';
+
 if(have_rows('text__image_list')):
-?>
-<section class="productTextImage">
-    <div class="container">
-        <div class="productTextImage__content">
-            <?php while(have_rows('text__image_list')): the_row(); 
-                $image_width = get_sub_field('image_width');
-                $title = get_sub_field('title');
-                $text = get_sub_field('text');
-                $image = get_sub_field('image');
-                if($image || $title || $text):
-                ?>
-                <div class="productTextImage__block">
-                    <?php if($image): ?>
-                        <div class="productTextImage__image image-<?php echo $image_width; ?>"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>"></div>
-                    <?php endif; ?>
-                    <?php if($title || $text): ?>
-                        <div class="productTextImage__textWrapper">
-                            <?php if($title): ?>
-                                <h4 class="productTextImage__title"><?php echo $title; ?></h4>
+    ?>
+    <section class="productTextImage <?php echo $spacing ?>">
+        <div class="container container--narrow">
+            <div class="productTextImage__content">
+                <?php
+                $index = 0;
+                while(have_rows('text__image_list')): the_row();
+                    $image_width = get_sub_field('image_width');
+                    $title = get_sub_field('title');
+                    $text = get_sub_field('text');
+                    $image = get_sub_field('image');
+                    if($image || $title || $text):
+                        ?>
+                        <div class="productTextImage__block productTextImage__block--<?php echo $index; ?>">
+                            <?php if($image): ?>
+                                <div class="productTextImage__image image-<?php echo $image_width; ?>"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>"></div>
                             <?php endif; ?>
-                            <?php if($text): ?>
-                                <div class="productTextImage__text"><?php echo $text; ?></div>
+                            <?php if($title || $text): ?>
+                                <div class="productTextImage__textWrapper">
+                                    <?php if($title): ?>
+                                        <h4><?php echo $title; ?></h4>
+                                    <?php endif; ?>
+                                    <?php if($text): ?>
+                                        <div class="productTextImage__text"><?php echo $text; ?></div>
+                                    <?php endif; ?>
+                                </div>
                             <?php endif; ?>
                         </div>
-                    <?php endif; ?>
-                </div>
-                <?php endif; ?>
-            <?php endwhile; ?>
+                        <?php
+                        $index++;
+                    endif;
+                endwhile;
+                ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php endif; ?>
