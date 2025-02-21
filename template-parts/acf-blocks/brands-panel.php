f3407f8648156059b98720857b688149303735a3
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-21 12:30:43 +0200

    fix (brands from cpt to taxonomy)

--- template-parts/acf-blocks/brands-panel.php
+++ template-parts/acf-blocks/brands-panel.php
@@ -1,27 +1,46 @@
-<?php 
-$args = array(
-    'post_type' => 'brand',
-    'posts_per_page' => -1,
-);
-$the_query = new WP_Query($args);
-if($the_query->have_posts()):
-?>
-<div class="brands">
-    <div class="container container--narrow">
-        <div class="manfacturers__list brands__list">
-            <?php while($the_query->have_posts()): $the_query->the_post(); ?>
-                <?php
-                $bg = get_field('brand__image') ? get_field('brand__image') : get_template_directory_uri() . '/assets/images/placeholder-image.webp';
-                $logo = get_the_post_thumbnail_url();
-                ?>
-                <a href="<?php the_permalink(); ?>" class="manfacturers__listItem">
-                    <img src="<?php echo $bg['url']; ?>" class="manfacturers__listItem__bg" alt="<?php echo $bg['title']; ?>">
-                    <?php if($logo): ?>
-                       <img src="<?php echo $logo; ?>" class="manfacturers__listItem__logo" alt="<?php echo $logo; ?>">
-                    <?php endif; ?>
-                </a>
-            <?php endwhile; wp_reset_postdata(); ?>
+<?php
+$terms = get_terms(array(
+    'taxonomy'   => 'products-brand',
+    'hide_empty' => false,
+));
+
+if (!empty($terms) && !is_wp_error($terms)): ?>
+    <div class="brands">
+        <div class="container container--narrow">
+            <div class="manfacturers__list brands__list">
+                <?php foreach ($terms as $term):
+                    $bg   = get_field('brand_bg_image', $term);
+                    $logo = get_field('brand_logo', $term);
+
+                    if (!$bg) {
+                        $bg = array(
+                            'url'   => get_template_directory_uri() . '/assets/images/placeholder-image.webp',
+                            'title' => 'Placeholder',
+                        );
+                    }
+
+                    $term_link = get_term_link($term);
+
+                    ?>
+                    <a href="<?php echo esc_url($term_link); ?>" class="manfacturers__listItem">
+                        <?php if (!empty($bg['url'])): ?>
+                            <img
+                                    src="<?php echo esc_url($bg['url']); ?>"
+                                    class="manfacturers__listItem__bg"
+                                    alt="<?php echo esc_attr($bg['title'] ?? $term->name); ?>"
+                            />
+                        <?php endif; ?>
+
+                        <?php if (!empty($logo['url'])): ?>
+                            <img
+                                    src="<?php echo esc_url($logo['url']); ?>"
+                                    class="manfacturers__listItem__logo"
+                                    alt="<?php echo esc_attr($logo['title'] ?? $term->name); ?>"
+                            />
+                        <?php endif; ?>
+                    </a>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
-</div>
 <?php endif; ?>
