25532df730a857a8d304c26fbaabd939eafeee24
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-21 12:10:40 +0200

    fix (hero brands slider form cpt to taxonomy)

--- template-parts/acf-blocks/hero-brands-slider.php
+++ template-parts/acf-blocks/hero-brands-slider.php
@@ -1,33 +1,50 @@
-<?php if(have_rows('brands_slider')): ?>
+<?php
+$brands_terms = get_sub_field('brands_terms');
+?>
+
+<?php if ($brands_terms): ?>
     <div class="heroBrands">
         <div class="container container--wide">
             <div class="heroBrands__wrapper">
                 <div class="heroBrands__slider heroBrands__slider--js">
-                    <?php while(have_rows('brands_slider')): the_row();
-                        $bg = get_sub_field('background_image');
-                        $logo = get_sub_field('logo');
-                        $description = get_sub_field('description');
-                        $link = get_sub_field('link');
-                        if($logo || $bg || $description || $link):
+                    <?php
+                    foreach ($brands_terms as $term):
+                        $bg          = get_field('brand_bg_image', $term);
+                        $logo        = get_field('brand_logo', $term);
+                        $description = $term->description;
+
+                        $link = [
+                            'url'   => get_term_link($term),
+                            'title' => $term->name,
+                        ];
+
+                        if($bg || $logo || $description || $link):
                             ?>
-                            <a href="<?php echo $link['url']; ?>" class="heroBrands__sliderItem__link">
+                            <a href="<?php echo esc_url($link['url']); ?>" class="heroBrands__sliderItem__link">
                                 <div class="heroBrands__sliderItem">
                                     <div class="heroBrands__inner">
                                         <div class="heroBrands__front">
                                             <?php if ($bg): ?>
-                                                <img class="heroBrands__bg" src="<?php echo get_custom_image($bg, 'custom_450x250'); ?>" alt="<?php echo esc_attr($bg['title']); ?>">
+                                                <img class="heroBrands__bg"
+                                                     src="<?php echo esc_url(get_custom_image($bg, 'custom_450x250')); ?>"
+                                                     alt="<?php echo esc_attr($bg['title'] ?? ''); ?>">
                                             <?php endif; ?>
 
-                                            <?php if($logo): ?>
-                                                <div class="heroBrands__logo"><img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['title']; ?>"></div>
+                                            <?php if ($logo): ?>
+                                                <div class="heroBrands__logo">
+                                                    <img src="<?php echo esc_url($logo['url']); ?>"
+                                                         alt="<?php echo esc_attr($logo['title'] ?? ''); ?>">
+                                                </div>
                                             <?php endif; ?>
                                         </div>
                                         <div class="heroBrands__backend">
-                                            <?php if($description): ?>
-                                                <div class="heroBrands__description"> <?php echo $description; ?> </div>
+                                            <?php if ($description): ?>
+                                                <div class="heroBrands__description">
+                                                    <?php echo esc_html($description); ?>
+                                                </div>
                                             <?php endif; ?>
                                             <div class="heroBrands__btn h6">
-                                                <?php echo $link['title']; ?>
+                                                <?php echo 'Find out more' ?>
                                                 <?php echo get_inline_svg('white-arrow'); ?>
                                             </div>
                                         </div>
@@ -35,8 +52,9 @@
                                 </div>
                             </a>
                         <?php endif; ?>
-                    <?php endwhile; ?>
+                    <?php endforeach; ?>
                 </div>
+
                 <div class="heroBrands__slider__prev">
                     <?php echo get_inline_svg('slider-arrow'); ?>
                 </div>
