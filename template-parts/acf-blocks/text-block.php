7dcf7c5dd292962cf199982b7acb2ba3a3bd0f2d
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-25 11:53:58 +0200

    feat (textBlock frontend)

--- template-parts/acf-blocks/text-block.php
+++ template-parts/acf-blocks/text-block.php
@@ -3,8 +3,8 @@ $title = get_sub_field('title');
 $text = get_sub_field('text');
 if($title || $text):
 ?>
-<section class="textBlock">
-    <div class="container">
+<section class="textBlock content-block">
+    <div class="container container--narrow">
         <div class="textBlock__content">
             <?php if($title): ?>
                 <h2 class="textBlock__title"><?php echo $title; ?></h2>
