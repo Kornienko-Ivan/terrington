3217a00b090faf5acf3bdf8a58762c59c6dd19dd
HlibPanchenko <glebpanchenk7@gmail.com>
2025-02-26 12:44:54 +0200

    feat (single taxonomy brand)

--- taxonomy-products-brand.php
+++ taxonomy-products-brand.php
@@ -1,2 +1,3 @@
 <?php get_header(); ?>
+<?php the_acf_loop(); ?>
 <?php get_footer(); ?>
--- template-parts/acf-blocks/hero.php
+++ template-parts/acf-blocks/hero.php
@@ -1,20 +1,29 @@
-<?php 
+<?php
 $title = get_sub_field('title');
 $bg_image = get_sub_field('background_image');
-?>
-<?php if($title || $bg_image): ?>
 
-<div class="hero">
-    <div class="hero__wrapper container container--wide">
-        <?php if($title): ?>
-            <h1 class="hero__title"><?php echo $title; ?></h1>
-        <?php endif; ?>
-        <?php if($bg_image): ?>
-            <div class="hero__image">
-                <img src="<?php echo esc_url(get_custom_image($bg_image, 'custom_1650x500')); ?>" alt="<?php echo $bg_image['title']; ?>">
-            </div>
-        <?php endif; ?>
+if (is_tax('products-brand')) {
+    $brand_logo = get_field('brand_logo', get_queried_object());
+}
+?>
+<?php if($title || $bg_image || $brand_logo): ?>
+
+    <div class="hero">
+        <div class="hero__wrapper container container--wide">
+            <?php if(is_tax('products-brand') && $brand_logo): ?>
+                <div class="hero__logo">
+                    <img src="<?php echo esc_url($brand_logo['url']); ?>" alt="<?php echo esc_attr($brand_logo['alt']); ?>">
+                </div>
+            <?php elseif($title): ?>
+                <h1 class="hero__title"><?php echo esc_html($title); ?></h1>
+            <?php endif; ?>
+
+            <?php if($bg_image): ?>
+                <div class="hero__image">
+                    <img src="<?php echo esc_url(get_custom_image($bg_image, 'custom_1650x500')); ?>" alt="<?php echo esc_attr($bg_image['title']); ?>">
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
 
 <?php endif; ?>
