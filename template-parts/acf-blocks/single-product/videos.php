5a9ad9414587aeb62c912c04871afbb4fbba4e9b
HlibPanchenko <glebpanchenk7@gmail.com>
2025-06-02 11:16:34 +0300

    fix: youtube video preview

--- template-parts/acf-blocks/single-product/videos.php
+++ template-parts/acf-blocks/single-product/videos.php
@@ -1,5 +1,18 @@
 <?php
 $title = get_sub_field('title');
+
+function get_best_youtube_thumbnail($video_id) {
+    $fallbacks = ['maxresdefault', 'sddefault', 'hqdefault', 'mqdefault', 'default'];
+    foreach ($fallbacks as $type) {
+        $url = "https://img.youtube.com/vi/{$video_id}/{$type}.jpg";
+        $headers = @get_headers($url);
+        if ($headers && strpos($headers[0], '200') !== false) {
+            return $url;
+        }
+    }
+    return ''; // none found
+}
+
 if($title || have_rows('videos_list')):
     ?>
     <section class="productVideos">
@@ -12,12 +25,18 @@ if($title || have_rows('videos_list')):
                     <div class="productVideos__list">
                         <?php while(have_rows('videos_list')): the_row();
                             $video = get_sub_field('video_link');
-                            if(!empty(explode('watch?v=', $video)[1])){
-                                $placeholder = explode('watch?v=', $video)[1];
-                            } else {
-                                $placeholder = explode('?list=', explode('youtu.be/', $video)[1])[0];
+                            $video_id = '';
+
+                            if (strpos($video, 'watch?v=') !== false) {
+                                $video_id = explode('watch?v=', $video)[1];
+                            } elseif (strpos($video, 'youtu.be/') !== false) {
+                                $video_id = explode('youtu.be/', $video)[1];
+                                $video_id = explode('?list=', $video_id)[0];
                             }
-                            $image = get_sub_field('video_preview') ? get_sub_field('video_preview')['url'] : 'https://img.youtube.com/vi/' . $placeholder . '/maxresdefault.jpg';
+
+                            $image = get_sub_field('video_preview')
+                                ? get_sub_field('video_preview')['url']
+                                : get_best_youtube_thumbnail($video_id);
                             if (!empty($image)):
                                 ?>
                                 <a href="<?php echo esc_url($video); ?>" class="productVideos__listItem" target="_blank" rel="noopener noreferrer">
