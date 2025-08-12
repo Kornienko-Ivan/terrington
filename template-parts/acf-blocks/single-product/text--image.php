6409fe5797dcc73aa2ad3ab36b27a0d8694318ed
HlibPanchenko <glebpanchenk7@gmail.com>
2025-08-12 15:48:44 +0300

    feat: text-image block

--- template-parts/acf-blocks/single-product/text--image.php
+++ template-parts/acf-blocks/single-product/text--image.php
@@ -9,7 +9,6 @@ if(have_rows('text__image_list')):
                 <?php
                 $index = 0;
                 while(have_rows('text__image_list')): the_row();
-                    $image_width = get_sub_field('image_width');
                     $title = get_sub_field('title');
                     $text = get_sub_field('text');
                     $image = get_sub_field('image');
@@ -17,7 +16,7 @@ if(have_rows('text__image_list')):
                         ?>
                         <div class="productTextImage__block productTextImage__block--<?php echo $index; ?>">
                             <?php if($image): ?>
-                                <div class="productTextImage__image image-<?php echo $image_width; ?>">
+                                <div class="productTextImage__image">
                                     <img src="<?php echo esc_url(get_custom_image($image, 'custom_500x500')); ?>" alt="<?php echo esc_attr($image['title']); ?>">
                                 </div>
                             <?php endif; ?>
